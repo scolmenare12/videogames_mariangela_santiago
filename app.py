2863af30d832a738291033cbc6244a7855f00fc8
Mariangela <mariangelalaya16@gmail.com>
2026-03-12 17:06:03 -0400

    feat: Creación del script para limpieza y exportación de los csv

---
+++ app.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+# Cargar el csv
+df = pd.read_csv('12. Videogame Sales.csv')
+
+# Mostrar información básica del df
+print(df.columns.tolist())
+print(df.head(10))
+
+# Eliminar columnas de ventas regionales ya que vamos a trabajar con las globales
+columnas_a_eliminar = ['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']
+columnas_existentes = [col for col in columnas_a_eliminar if col in df.columns]
+df_limpio = df.drop(columns=columnas_existentes)
+
+# Renombrar Global Sales para denotar medida (Millones)
+df_limpio = df_limpio.rename(columns={'Global_Sales': 'Global_Sales_Millions'})
+
+# Eliminar filas duplicadas
+filas_antes = len(df_limpio)
+df_limpio = df_limpio.drop_duplicates()
+filas_despues = filas_antes - len(df_limpio)
+
+print(f"Filas duplicadas eliminadas: {filas_despues}")
+
+# En este caso hay 0 filas duplicadas
+
+print(df_limpio.columns.tolist())
+
+publisher_df = (
+    df_limpio.groupby('Publisher')
+    .agg({
+        'Name': 'count',  # Contar número de juegos
+        'Global_Sales_Millions': 'sum'  # Suma de total de ventas
+    })
+)
+
+# Renombrar columnas y devolver Publisher como columna normal
+publisher_df = (
+    publisher_df
+    .rename(columns={
+        'Name': 'Total_Games',
+        'Global_Sales_Millions': 'Total_Sales_Millions'
+    })
+    .reset_index()
+)
+
+# Ordenar por Total_Games de mayor a menor
+top_publisher_df = publisher_df.sort_values(by='Total_Games', ascending=False).reset_index(drop=True)
+
+# Crear columna de ID
+top_publisher_df.insert(0, 'ID', range(1, len(top_publisher_df) + 1))
+
+# Seleccion de las columnas para el DF top_publishers
+top_publishers = top_publisher_df[['ID', 'Publisher', 'Total_Games']].copy()
+
+# Ordenar por Total_Sales_Millions de mayor a menor
+top_publishers_sales_df = publisher_df.sort_values(by='Total_Sales_Millions', ascending=False).reset_index(drop=True)
+
+# Crear columna de ID
+top_publishers_sales_df.insert(0, 'ID', range(1, len(top_publishers_sales_df) + 1))
+
+# Seleccion de las columnas para el DF top_publishers_sales
+top_publishers_sales = top_publishers_sales_df[['ID', 'Publisher', 'Total_Sales_Millions']].copy()
+
+# Guardar los dataframes en csv
+df_limpio.to_csv('videogame_sales.csv', index=False)
+top_publishers.to_csv('top_publishers.csv', index=False)
+top_publishers_sales.to_csv('top_publishers_sales.csv', index=False)
